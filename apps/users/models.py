f2d6e931c0602fbeaf4432d5c053c4e3a9925c0f
behruz171 <behruzzo662@gmail.com>
2024-03-08 18:06:54 +0500

    nimadir

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,2 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.core import validators
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
+
+
+@deconstructible
+class PhoneValidator(validators.RegexValidator):
+    regex = r"^"
+    message = _(
+        "Enter a valid username. This value may contain only letters, "
+        "numbers, and @/./+/-/_ characters."
+    )
+    flags = 0
+
+class User(AbstractUser):
+    phone_validator = PhoneValidator()
+
+    phone_number = models.CharField(
+        max_length = 13,
+        verbose_name = "Phone number",
+        validators = [phone_validator],
+        unique=True,
+        error_messages = {
+            "unique": "Bunday telefon raqam allaqachon mavjud!"
+        }
+    )
+
